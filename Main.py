bb22095efd2a7c9d28dce04331babaf3823cb0bc
vl05953 <adam.szilagyi@ksh.hu>
2025-09-30 09:43:20 +0200

    Add first files

---
+++ Main.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import os
+from openai import OpenAI
+from streamlit_option_menu import option_menu
+from datetime import datetime
+import urllib
+import pandas as pd
+
+answers = pd.DataFrame(columns = ['Model', 'Question', 'Answer'])
+
+st.set_page_config(
+  layout = 'wide',
+  page_title = 'Ask AI with Python',
+  page_icon = 'https://map.ksh.hu/timea/images/shortcut.ico',
+  menu_items = {'Get help': 'mailto:adam.szilagyi@ksh.hu',
+                'Report a bug': 'mailto:adam.szilagyi@ksh.hu',
+                'About': 'This webapplication makes you able to chat and generate picture with ChatGPT through OpenAI module.'}
+  )
+  
+st.title('Ask AI with Py')
+
+selected = option_menu(None, ['Chat', 'Image'], menu_icon = 'cast', default_index = 0, orientation = 'horizontal')
+
+if selected == 'Chat': 
+  password = st.text_input('Set your OpenAI API key:', type = 'password', value = os.environ['OPENAI_API_KEY'], placeholder = "If you don't have one, then you can create here: https://platform.openai.com/api-keys")
+  model = st.selectbox('Choose AI Model:', options = ['gpt-5', 'gpt-5-mini', 'gpt-5-nano'])
+  question = st.text_area('Write here your question:', placeholder = 'Ask something!')
+  if st.button('Answer me!'): 
+    try:
+      client = OpenAI(api_key = password)
+      model = model # os.environ['OPENAI_MODEL']
+      response = client.chat.completions.create(model = model, messages = [{"role": "user", "content": question},])
+      answer = response.choices[0].message.content.strip()
+      st.text(answer)
+      now = datetime.now().strftime('%Y%m%d%H%M%S')
+      filename = 'Chat' + now + '.txt'
+      # answers = pd.concat([model, question, answer])
+      answers.loc[len(answers)] = [model, question, answer]
+      st.download_button(label = 'Download Chat', data = answers.to_csv(index = False).encode('utf-8'), file_name = filename) # ';'.join([model, mquestion, answer])
+    except Exception as e:
+      st.error(f'An Error happened: {e}')
+  
+  
+elif selected == 'Image':
+  password2 = st.text_input('Set your OpenAI API key:', type = 'password', value = os.environ['OPENAI_API_KEY'], placeholder = "If you don't have one, then you can create here: https://platform.openai.com/api-keys")
+  model2 = st.selectbox('Choose AI Model:', options = ['dall-e-3', 'gpt-image-1', 'gpt-image-0721-mini-alpha', 'dall-e-2'])
+  desciption = st.text_area('What should the picture depict?', placeholder = 'Describe here...')
+  if st.button('Draw me!'):
+    try:
+      client = OpenAI(api_key = password2)
+      # model = 'dall-e-3' # 'gpt-image-1', 'gpt-image-0721-mini-alpha', 'dall-e-2', and 'dall-e-3'
+        
+      response = client.images.generate(
+        model = model2, 
+        prompt = desciption, 
+      )
+        
+      now = datetime.now().strftime('%Y%m%d%H%M%S')
+      filename = 'image' + now + '.png'
+      urllib.request.urlretrieve(response.data[0].url, filename)
+      st.image(response.data[0].url)
+    except Exception as e:
+      st.error(f'An Error happened: {e}')
+  
